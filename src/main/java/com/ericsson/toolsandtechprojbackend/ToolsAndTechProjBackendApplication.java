269a9cff17c3229c8088cb7e47ab06ade7d3b7de
Damian Chrzanowski <pjdamian.chrzanowski@gmail.com>
2026-05-07 10:13:21 +0100

    Initial commit

---
+++ src/main/java/com/ericsson/toolsandtechprojbackend/ToolsAndTechProjBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ericsson.toolsandtechprojbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsAndTechProjBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolsAndTechProjBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ericsson/toolsandtechprojbackend/ToolsAndTechProjBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericsson.toolsandtechprojbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsAndTechProjBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
